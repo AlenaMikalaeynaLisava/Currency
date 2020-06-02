ca452e679fe725f52bfb74dc9c04ecd764fd70b0
AlenaMikalaeynaLisava <63248586+AlenaMikalaeynaLisava@users.noreply.github.com>
2020-06-02 16:02:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-//let currencyId =[298, 145, 292];
+let currencyId =[0, 298, 145, 292];
+let marker=0;
 async function currencyExchenge(id){
   const url =id
   ? 'https://www.nbrb.by/api/exrates/rates/' +id
@@ -9,6 +10,7 @@ async function currencyExchenge(id){
   return fetcheData;
  }
  let date;
+
 
  Promise.all([
    0,
@@ -25,12 +27,20 @@ async function currencyExchenge(id){
   let value;
   let abbreviation;
   let CurrencySelect = myForm.language; 
+  //let marker=0;
   function changeOption(){
+    marker=1;
     let selectedOption = CurrencySelect.options[CurrencySelect.selectedIndex];
     for (i=0; i< data.length; i++){
       if(option[i].innerText ==selectedOption.text)
       {value = option[i].value;
         abbreviation = option[i].innerText;}
+    }
+    if(byn.value){
+      button.removeAttribute('disabled'); 
+    }
+    if(abbreviation == "Select currency"){
+      button.disabled = true;
     }
 }
 CurrencySelect.addEventListener("change", changeOption);
@@ -39,27 +49,48 @@ date = new Date(date);
 let day = date.getDate();
 let month =  date.getMonth();
 let yaer = date.getFullYear();
-   button.addEventListener('click',(event)=>{
+
+byn.addEventListener('change',(event)=>{
+  if(byn.value && marker==1){
+    button.removeAttribute('disabled'); 
+  } 
+})
+byn.addEventListener('change',(event)=>{
+  if(!byn.value){
+    button.disabled = true;
+  } 
+})
+
+  button.addEventListener('click',(event)=>{
     if(byn.value && (abbreviation != "Select currency")){
       bynValue=byn.value;
       let val1= parseFloat(value).toFixed(2);
      result.innerText= "You can buy " +(bynValue/value).toFixed(2) + " " + abbreviation + " for " + bynValue +" BYN with  rate " + val1 + " for 1 ye on " + day+"/"+ month+"/"+yaer;
-    } else {
-      button.disabled = true;
     }
    })
-  
  })
 let option =[];
 let selection = document.getElementById('selection');
 let button = document.getElementById('button');
+button.disabled = true;
 let byn = document.getElementById('byn');
 let result = document.getElementById('result');
-for (i=0; i<4; i++) {
+for (i=0; i<currencyId.length; i++) {
   option[i] = document.createElement('option');
   option[i].innerText = i;
   selection.append(option[i]);
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
